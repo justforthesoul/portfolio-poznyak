8648fa4096bb75e941e5459c72368b0ffc25f9ec
Poznyak_Pavel_Viktorovich <poznyak8230@gmail.com>
2019-07-02 16:39:52 +0200

    start commit

---
+++ src/scripts/parallax-mountain.js
@@ -0,0 +1,15 @@
+const parallaxMountain = document.querySelector(".parallax-mountain");
+const mountainLayers = parallaxMountain.querySelectorAll(".parallax-mountain__layer");
+console.log(mountainLayers);
+function moveMoutaisLayerOnScroll (wScroll) {
+    mountainLayers.forEach(layer => {
+        const divider = layer.dataset.speed;
+        const strafe = wScroll * divider / 900;
+        layer.style.transform = `translateY(-${strafe}%)`;
+    } );
+}
+
+window.addEventListener("scroll", e => {
+    const wScroll = window.pageYOffset;
+    moveMoutaisLayerOnScroll(wScroll);
+});
