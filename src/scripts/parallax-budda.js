e068eb49966c2a9c3d9a105acff7b5acf97b91dc
Poznyak_Pavel_Viktorovich <poznyak8230@gmail.com>
2019-07-10 17:34:35 +0200

    добавил анимашку для тряски мыши

--- src/scripts/parallax-budda.js
+++ src/scripts/parallax-budda.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
     buddaLayers.forEach(layer => {
       const divider = layer.dataset.speed;
       const strafeX = (wScrollX * divider) / 7000;
-      const strafeY = (wScrollY * divider) / 3000;
+      const strafeY = (wScrollY * divider) / 5000;
       layer.style.transform = `translate(-${strafeX}%, -${strafeY}%)`;
     });
   }
